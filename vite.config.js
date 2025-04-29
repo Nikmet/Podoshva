d8f35e144b7a2666704cff30d0e3900d20d5fe34
nikmet <metlov.nm@yandex.ru>
2025-04-29 09:33:08 +0300

    asdasd

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/podoshva-market.vercel.app/'
+  base: 'https://podoshva-market.vercel.app/'
 })
